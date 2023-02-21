fce43cf9c3c92e4bbb2e2ba54d5d375470fffc8a
Will Roden <will@roden.cc>
2023-02-21 17:16:25 -0600

    WIP

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import {
   ExecuteFunctionResponse,
   ListFunctionsResponse,
   PluginServiceDefinition,
-  PluginServiceImplementation,
 } from '../gen/proto/ts/plugin/v1/plugin';
 
 // Options for executing a template.
@@ -35,6 +34,9 @@ interface GotmplOptions {
   functions?: {[name: string]: Function};
 }
 
+// Runs `gotmpl exec` with the given template and options. This is useful when you only have a few templates to execute
+// and don't want the overhead of starting a server. If you have a lot of templates or need to execute them
+// concurrently, you should use gotmplServer instead.
 export async function gotmplExec(
   template: string,
   options: GotmplOptions & GotmplExecOptions = {}
@@ -48,18 +50,7 @@ export async function gotmplExec(
     '--package',
     options.package || 'text',
   ];
-  if (options.plugins) {
-    for (const plugin of options.plugins) {
-      args.push('--plugin', plugin);
-    }
-  }
-  let pluginServer: ReturnType<typeof createServer> | undefined;
-  if (options.functions && Object.keys(options.functions).length > 0) {
-    pluginServer = createServer();
-    pluginServer.add(PluginServiceDefinition, new Plugin(options.functions));
-    const pluginServerPort = await pluginServer.listen('localhost:0');
-    args.push('--grpc-plugin', `localhost:${pluginServerPort}`);
-  }
+  const pluginServer = await handleCommonOptions(options, args);
   let result = '';
   try {
     const bin = options.gotmplBin || 'gotmpl';
@@ -85,13 +76,14 @@ export async function gotmplExec(
 
 interface GotmplServer {
   execute(template: string, options: GotmplExecOptions): Promise<string>;
+
   stop(): Promise<void>;
 }
 
-export async function gotmplServer(
-  options: GotmplOptions = {}
-): Promise<GotmplServer> {
-  const args = ['server', '--address', 'localhost:0'];
+async function handleCommonOptions(
+  options: GotmplOptions,
+  args: string[]
+): Promise<ReturnType<typeof createServer> | undefined> {
   for (const plugin of options.plugins || []) {
     args.push('--plugin', plugin);
   }
@@ -102,6 +94,18 @@ export async function gotmplServer(
     const pluginServerPort = await pluginServer.listen('localhost:0');
     args.push('--grpc-plugin', `localhost:${pluginServerPort}`);
   }
+  return pluginServer;
+}
+
+// Runs `gotmpl server` and returns a client for executing templates. This is useful when you have a lot of templates
+// or need to execute them concurrently. If you only have a few templates to execute, you should use gotmplExec instead.
+// The returned server must be stopped with the stop method when you are done with it. After stopping, calls to
+// execute will fail.
+export async function gotmplServer(
+  options: GotmplOptions = {}
+): Promise<GotmplServer> {
+  const args = ['server', '--address', 'localhost:0'];
+  const pluginServer = await handleCommonOptions(options, args);
   const process = spawn(options.gotmplBin || 'gotmpl', args);
   return new Promise(resolve => {
     process.stdout.on('data', data => {
@@ -142,7 +146,7 @@ export async function gotmplServer(
   });
 }
 
-class Plugin implements PluginServiceImplementation {
+class Plugin {
   constructor(private readonly functions: {[name: string]: Function}) {}
 
   async listFunctions(): Promise<ListFunctionsResponse> {
